a9833ef588134f057bd3a3e2b0e7a95449296356
Si3nLoong <sianloong90@gmail.com>
2021-02-06 12:12:59 +0800

    feat(minify): generate minify version of bundle file

--- scripts/build.js
+++ scripts/build.js
@@ -7,7 +7,8 @@ import svelte from "rollup-plugin-svelte";
 import resolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 import commonjs from "@rollup/plugin-commonjs";
-// import css from "rollup-plugin-css-only";
+import { terser } from "rollup-plugin-terser";
+import { babel } from "@rollup/plugin-babel";
 import sveltePreprocess from "svelte-preprocess";
 
 const extractCss = () => {
@@ -102,10 +103,43 @@ const extractCss = () => {
 };
 
 const production = !process.env.ROLLUP_WATCH;
+const rollupPlugins = [
+  svelte({
+    compilerOptions: {
+      // enable run-time checks when not in production
+      dev: !production,
+    },
+    emitCss: true,
+    // we'll extract any component CSS out into
+    // a separate file - better for performance
+    preprocess: sveltePreprocess(),
+  }),
+  extractCss(),
+  // css({ output: "index.css" }),
+  resolve({
+    preferBuiltins: true,
+    browser: true,
+    dedupe: ["svelte"],
+  }),
+  commonjs(),
+  typescript({
+    sourceMap: false,
+  }),
+];
 
-const analyzePackageJson = async (bundle, filepath, pkg) => {
+const analyzePackageJson = async (filepath, pkg) => {
+  let bundle = await rollup({
+    input: path.resolve(`${filepath}/${pkg.svelte}`),
+    plugins: rollupPlugins,
+  });
+
+  const name = camelCase(pkg.name.replace("@responsive-ui/", ""), {
+    pascalCase: true,
+  });
+
   const files = [pkg.main, pkg.module, pkg.browser];
   const map = new Map();
+
   for (let i = 0; i < files.length; i++) {
     const file = files[i];
     if (!file) continue;
@@ -113,10 +147,6 @@ const analyzePackageJson = async (bundle, filepath, pkg) => {
     const filename = path.basename(file);
     map.set(filename, true);
     if (map.has(file)) continue;
-
-    const name = camelCase(pkg.name.replace("@responsive-ui/", ""), {
-      pascalCase: true,
-    });
 
     let result = {
       file: filename,
@@ -140,6 +170,7 @@ const analyzePackageJson = async (bundle, filepath, pkg) => {
     for (const chunk of output) {
       const { fileName } = chunk;
       const outputPath = `${filepath}/${path.dirname(file)}/${fileName}`;
+      console.log(chalk.green(outputPath));
       if (chunk.type === "asset") {
         fs.outputFileSync(outputPath, chunk.source);
       } else {
@@ -147,6 +178,53 @@ const analyzePackageJson = async (bundle, filepath, pkg) => {
       }
     }
   }
+
+  // generate
+  bundle = await rollup({
+    input: path.resolve(`${filepath}/${pkg.svelte}`),
+    plugins: [
+      ...rollupPlugins,
+      babel({
+        extensions: [".js", ".mjs", ".ts", ".html", ".svelte"],
+        babelHelpers: "runtime",
+        exclude: ["node_modules/@babel/**"],
+        presets: [
+          [
+            "@babel/preset-env",
+            {
+              targets: "> 0.25%, not dead",
+            },
+          ],
+        ],
+        plugins: [
+          "@babel/plugin-syntax-dynamic-import",
+          // "@babel/plugin-syntax-import-meta",
+          [
+            "@babel/plugin-transform-runtime",
+            {
+              useESModules: true,
+            },
+          ],
+        ],
+      }),
+      terser({
+        module: true,
+      }),
+    ],
+  });
+
+  console.log("Generating minify version of js.");
+  const fileName = "index.min.js";
+  const { output } = await bundle.generate({
+    format: "iife",
+    name,
+    file: fileName,
+  });
+
+  const chunk = output.shift();
+  const outputPath = `${filepath}/lib/${fileName}`;
+  fs.outputFileSync(outputPath, chunk.code);
+  console.log("Generated successful!", chalk.green(outputPath));
 };
 
 // const excludedFolders = [".ds_store", "lib"];
@@ -174,7 +252,7 @@ const analyzePackageJson = async (bundle, filepath, pkg) => {
       //   continue;
       // }
 
-      console.log(chalk.green(file));
+      console.log(`Bundling for component ${chalk.green(file)}`);
       const basePath = `${pkgPath}/${file}`;
       const pkg = JSON.parse(
         fs.readFileSync(path.resolve(`${basePath}/package.json`), "utf8")
@@ -184,34 +262,7 @@ const analyzePackageJson = async (bundle, filepath, pkg) => {
         fs.rm(`${pkgPath}/${file}/lib`, { recursive: true, force: true });
       }
 
-      const bundle = await rollup({
-        input: path.resolve(`${basePath}/${pkg.svelte}`),
-        plugins: [
-          svelte({
-            compilerOptions: {
-              // enable run-time checks when not in production
-              dev: !production,
-            },
-            emitCss: true,
-            // we'll extract any component CSS out into
-            // a separate file - better for performance
-            preprocess: sveltePreprocess(),
-          }),
-          extractCss(),
-          // css({ output: "index.css" }),
-          resolve({
-            preferBuiltins: true,
-            browser: true,
-            dedupe: ["svelte"],
-          }),
-          commonjs(),
-          typescript({
-            sourceMap: false,
-          }),
-        ],
-      });
-
-      await analyzePackageJson(bundle, basePath, pkg);
+      await analyzePackageJson(basePath, pkg);
     }
   }
 })();
