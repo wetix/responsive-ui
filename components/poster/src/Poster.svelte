ec179dd037e00d89442e43b4af807dcd9314a778
Si3nLoong <sianloong90@gmail.com>
2021-04-20 14:27:10 +0800

    BREAKING CHANGE: refactor poster APIs

--- components/poster/src/Poster.svelte
+++ components/poster/src/Poster.svelte
@@ -2,11 +2,10 @@
   let className = "";
   export { className as class };
   export let src = "";
-  export let width = "110px";
-  export let height = "150px";
+  export let responsive = true;
   export let shadowed = true;
   export let rounded = true;
-  export let size = "middle";
+  // export let size = "xs";
   export let style = "";
 
   let opacity = 1;
@@ -14,26 +13,15 @@
   img.onload = () => {
     opacity = 0;
   };
-
-  $: {
-    opacity = 1;
-    img.src = src;
-  }
-
-  if (size === "small") {
-    width = "80px";
-    height = "115px";
-  } else if (size === "extra-small") {
-    width = "70px";
-    height = "100px";
-  }
+  img.src = src;
 </script>
 
 <div
   class="responsive-ui-poster {className}"
+  class:responsive-ui-poster--responsive={responsive}
   class:responsive-ui-poster--shadowed={shadowed}
   class:responsive-ui-poster--rounded={rounded}
-  style={`background-image:url(${src});width:${width};height:${height};${style}`}
+  style={`background-image:url(${src});${style}`}
   on:click
 >
   <div class="responsive-ui-poster__overlay" style={`opacity:${opacity};`} />
@@ -47,6 +35,8 @@
     background-position: center;
     background-color: #f5f5f5;
     overflow: hidden;
+    width: 110px;
+    height: 150px;
 
     &--rounded {
       border-radius: var(--border-radius-sm, 3px);
@@ -67,6 +57,18 @@
       transition: opacity 0.5s ease;
       border-radius: var(--border-radius-sm, 3px);
       z-index: 1;
+    }
+
+    &--responsive {
+      @media screen and (min-width: 640px) {
+        width: 147px;
+        height: 210px;
+      }
+
+      @media screen and (min-width: 1024px) {
+        width: 203px;
+        height: 290px;
+      }
     }
   }
 </style>
