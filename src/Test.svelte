5065b61af2a39cec6dffac9cc1ea513e23a2cc68
si3nloong <sianloong90@gmail.com>
2021-10-26 10:36:21 +0800

    fix: remove postcss

--- src/Test.svelte
+++ src/Test.svelte
@@ -3,9 +3,17 @@
   import Button from "@responsive-ui/button/src/Button.svelte";
   import Modal from "@responsive-ui/modal/src/Modal.svelte";
   import DateRangePicker from "@responsive-ui/date-picker/src/DateRangePicker.svelte";
+  import Docker from "../components/dock/src/Dock.svelte";
+
+  let showModal = true;
+  let showDocker = false;
 </script>
 
-<Modal>
+<Modal
+  bind:open={showModal}
+  on:confirm={() => (showModal = false)}
+  on:cancel={() => (showModal = false)}
+>
   Lorem Ipsum is simply dummy text of the printing and typesetting industry.
   Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
   when an unknown printer took a galley of type and scrambled it to make a type
@@ -19,12 +27,15 @@
   <Date />
   <div>
     <Button size="small">Test 1</Button>
-    <Button>Test 2</Button>
+    <Button variant="primary">Test 1</Button>
+    <Button on:click={() => (showDocker = true)}>Test 2</Button>
   </div>
   <div>
     <DateRangePicker />
   </div>
 </div>
+
+<Docker bind:open={showDocker} />
 
 <style lang="scss">
   @import url("https://fonts.googleapis.com/css?family=Poppins:400,500,600,700&display=swap");
