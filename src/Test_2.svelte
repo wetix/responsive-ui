d32105b8ad16e3e8fbd4ca43a0467763a5b324e5
Mohamad Harith <mohamadharith.my@gmail.com>
2021-11-02 08:59:59 +0800

    chore: draft RangeSlider component

--- src/Test_2.svelte
+++ src/Test_2.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import AppBar from "../components/app-bar/src/AppBar.svelte";
+  import RangeSlider from "../components/range-slider/src/RangeSlider.svelte";
 
   const tabItems = [
     { value: "PERSONALDETAILS", title: "Personal Details" },
@@ -30,6 +31,20 @@
     // { value: "Settings", icon: "settings" },
     { value: "Logout", icon: "logout", link: `/logout` },
   ];
+
+  let lowSlideValue: string;
+  let upSlideValue: string;
 </script>
 
 <AppBar {centerItems} {tabItems} {dropdownItems} />
+
+<div style="padding:150px 10px 10px 10px">
+  <RangeSlider
+    min="100"
+    max="1000"
+    bind:lowerVal={lowSlideValue}
+    bind:upperVal={upSlideValue}
+  />
+  <p>Low: {lowSlideValue}</p>
+  <p>Up: {upSlideValue}</p>
+</div>
