44bf7b2e86635359f210301ead098de361036534
si3nloong <sianloong90@gmail.com>
2021-11-25 18:27:16 +0800

    ✅ chore: add stories for `Chip` and `FAB` components

---
+++ stories/Chip.stories.svelte
@@ -0,0 +1,28 @@
+<script>
+  import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
+  import Chip from "../components/chip/src/Chip.svelte";
+</script>
+
+<Meta
+  title="Components/Chip"
+  component={Chip}
+  argTypes={{
+    content: { control: "text" },
+    disabled: { control: "boolean" },
+    checked: { control: "boolean" },
+    onClick: { action: "onClick" },
+  }}
+/>
+
+<Template let:args>
+  <Chip {...args} on:click={args.onClick}>
+    {args.content}
+  </Chip>
+</Template>
+
+<Story
+  name="Default"
+  args={{
+    content: "Hello world!",
+  }}
+/>
