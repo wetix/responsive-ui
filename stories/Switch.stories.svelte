26fa272f719d30fd5c8c56475ec4bb4b50956ab9
si3nloong <sianloong90@gmail.com>
2021-12-04 23:33:42 +0800

    ✅ test: added stories

---
+++ stories/Switch.stories.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
+  import Switch from "@responsive-ui/switch";
+</script>
+
+<Meta
+  title="Components/Switch"
+  component={Switch}
+  argTypes={{
+    disabled: { control: "boolean" },
+    checked: { control: "boolean" },
+  }}
+/>
+
+<Template let:args>
+  <Switch {...args} on:click={args.onClick} />
+</Template>
+
+<Story name="Default" />
