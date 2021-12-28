e993b3e0a3a06912801e6b311951222d85935d4c
si3nloong <sianloong90@gmail.com>
2021-12-28 12:15:12 +0800

    💄 fix: update `AppBar` CSS

--- components/app-bar/src/AppBar.svelte
+++ components/app-bar/src/AppBar.svelte
@@ -1,12 +1,7 @@
 <script lang="ts">
   import { fade } from "svelte/transition";
-  import { tweened } from "svelte/motion";
   import Scroll from "@responsive-ui/hscroll";
   import type { NavItem } from "../types";
-
-  const tween = tweened(1, {
-    duration: 150,
-  });
 
   export let menuCaption = "";
   export let clientHeight = 0;
@@ -16,12 +11,6 @@
   export let trailingItems: NavItem[] = [];
 
   let openMenu = false;
-
-  $: if (openMenu) {
-    tween.set(0);
-  } else {
-    tween.set(1);
-  }
 
   const handleMenu = (e: Event) => {
     // if the element is underneath anchor link
@@ -110,30 +99,30 @@
     out:fade
     on:click={() => (openMenu = false)}
   />
-  <aside
-    class="resp-app-bar__menu"
-    style={`transform: translateX(-${$tween * 100}%);`}
-    on:click={handleMenu}
-  >
-    <header class="resp-app-bar__menu-header">
-      <caption>{menuCaption}</caption>
-      <i class="resp-app-bar__menu-icon" on:click={() => (openMenu = false)}>
-        {@html `<svg viewBox="64 64 896 896" focusable="false" data-icon="close" width="1em" height="1em" fill="currentColor" aria-hidden="true"><path d="M563.8 512l262.5-312.9c4.4-5.2.7-13.1-6.1-13.1h-79.8c-4.7 0-9.2 2.1-12.3 5.7L511.6 449.8 295.1 191.7c-3-3.6-7.5-5.7-12.3-5.7H203c-6.8 0-10.5 7.9-6.1 13.1L459.4 512 196.9 824.9A7.95 7.95 0 00203 838h79.8c4.7 0 9.2-2.1 12.3-5.7l216.5-258.1 216.5 258.1c3 3.6 7.5 5.7 12.3 5.7h79.8c6.8 0 10.5-7.9 6.1-13.1L563.8 512z" /></svg>`}
-      </i>
-    </header>
-    <div class="resp-app-bar__menu-body">
-      <ul>
-        {#each menuItems as { href, label, selected, ...otherProps }, index}
-          <li>
-            <slot name="menu-item" item={menuItems[index]} {index} {selected}>
-              <a {href} {...otherProps}>{label}</a>
-            </slot>
-          </li>
-        {/each}
-      </ul>
-    </div>
-  </aside>
 {/if}
+<aside
+  class="resp-app-bar__menu"
+  class:resp-app-bar__menu--close={!openMenu}
+  on:click={handleMenu}
+>
+  <header class="resp-app-bar__menu-header">
+    <caption>{menuCaption}</caption>
+    <i class="resp-app-bar__menu-icon" on:click={() => (openMenu = false)}>
+      {@html `<svg viewBox="64 64 896 896" focusable="false" data-icon="close" width="1em" height="1em" fill="currentColor" aria-hidden="true"><path d="M563.8 512l262.5-312.9c4.4-5.2.7-13.1-6.1-13.1h-79.8c-4.7 0-9.2 2.1-12.3 5.7L511.6 449.8 295.1 191.7c-3-3.6-7.5-5.7-12.3-5.7H203c-6.8 0-10.5 7.9-6.1 13.1L459.4 512 196.9 824.9A7.95 7.95 0 00203 838h79.8c4.7 0 9.2-2.1 12.3-5.7l216.5-258.1 216.5 258.1c3 3.6 7.5 5.7 12.3 5.7h79.8c6.8 0 10.5-7.9 6.1-13.1L563.8 512z" /></svg>`}
+    </i>
+  </header>
+  <div class="resp-app-bar__menu-body">
+    <ul>
+      {#each menuItems as { href, label, selected, ...otherProps }, index}
+        <li>
+          <slot name="menu-item" item={menuItems[index]} {index} {selected}>
+            <a {href} {...otherProps}>{label}</a>
+          </slot>
+        </li>
+      {/each}
+    </ul>
+  </div>
+</aside>
 
 <style lang="scss" global>
   $sm: 576px;
@@ -256,6 +245,12 @@
     &__menu {
       background: #fff;
       width: 90%;
+      transform: translateX(0%);
+      transition: transform 0.5s;
+
+      &--close {
+        transform: translateX(-110%);
+      }
 
       &-header {
         display: flex;
@@ -306,6 +301,7 @@
   .resp-app-bar__menu {
     a {
       cursor: pointer;
+      color: inherit;
       text-decoration: none;
     }
   }
