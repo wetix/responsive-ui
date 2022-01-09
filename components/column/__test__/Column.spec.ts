5ba287dffdc20b64857e016ea4dfaf8cbbbcfad2
Ryan Tan <ryan.tan@revenuemonster.my>
2022-01-09 22:00:52 +0800

    added test cases

--- components/column/__test__/Column.spec.ts
+++ components/column/__test__/Column.spec.ts
@@ -5,11 +5,32 @@ import SlotTest from "../../SlotTest/SlotTest.svelte";
 describe("Column", () => {
   const props = {
     id: "col",
+    class: "col-custom",
     title: "col title",
-    placeholder: "Select date"
+    placeholder: "Select date",
+    md: 2,
+    style: "width: 100%;"
   };
 
   it("should render correctly", () => {
-    const { component } = render(Column, { props });
+    const result = render(Column, { props });
+    expect(() => result).not.toThrow();
+  });
+
+  it("should render props correctly", () => {
+    const result = render(SlotTest, {Component: Column});
+    expect(() => result).not.toThrow();
+  });
+
+  it("prop test", () => {
+    const {container} = render(Column, { props });
+    const column = container.querySelector("." + props.class) as HTMLElement;
+
+    expect(column.getAttribute("id")).toEqual(props.id); //test id
+    expect(column.getAttribute("title")).toEqual(props.title); //test title
+    expect(column.classList).toContain("resp-col--md-2"); //test md
+    expect(column.getAttribute("style")).toEqual(props.style); //test style
+    //test placeholder
+    expect(column.getAttribute("placeholder")).toEqual(props.placeholder);
   });
 });
