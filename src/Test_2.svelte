50bd42502429894b97618e7ed0ae831f6c68bb20
si3nloong <sianloong90@gmail.com>
2022-01-21 20:25:37 +0800

    fix: `Quantity` component onchange event is not working

--- src/Test_2.svelte
+++ src/Test_2.svelte
@@ -1,49 +1,22 @@
 <script lang="ts">
-  import AppBar from "../components/app-bar/src/AppBar.svelte";
-  import Modal from "../components/modal/src/Modal.svelte";
+  import Quantity from "@responsive-ui/quantity";
+  import Modal from "@responsive-ui/modal";
   import RangeSlider from "../components/range/src/Range.svelte";
-
-  const tabItems = [
-    { value: "PERSONALDETAILS", title: "Personal Details" },
-    { value: "PURCHASEHISTORY", title: "Purchase History" },
-    { value: "PURCHASEHISTORY", title: "Purchase History" },
-    { value: "PURCHASEHISTORY", title: "Purchase History" },
-    { value: "PURCHASEHISTORY", title: "Purchase History" },
-    { value: "PURCHASEHISTORY", title: "Purchase History" },
-  ];
-
-  let centerItems = [
-    { value: "Movie", link: "/movie" },
-    { value: "Store", link: "/store" },
-    { value: "Blog", link: "https://articles.wetix.my/" },
-    // { value: "Promotion", link: "/promotion" },
-  ];
-
-  let dropdownItems = [
-    { value: "Profile", icon: "profile", link: `/me` },
-    {
-      value: "Purchase History",
-      icon: "history",
-      link: `/me/purchase-history`,
-    },
-    // { value: "My Tickets", icon: "ticket" },
-    // { value: "My Vouchers", icon: "voucher" },
-    // { value: "Loyalty", icon: "loyalty" },
-    // { value: "Settings", icon: "settings" },
-    { value: "Logout", icon: "logout", link: `/logout` },
-  ];
 
   let lowSlideValue: string;
   let upSlideValue: string;
 
   let openModal = true;
+
+  const onChange = (e: Event | undefined) => {
+    console.log((e!.currentTarget as HTMLInputElement).value);
+  };
 </script>
 
 <!-- <AppBar {centerItems} {tabItems} {dropdownItems} /> -->
 
-<Modal bind:open={openModal} on:cancel={() => (openModal = false)}
-  >Testing</Modal
->
+<Quantity min={2} max={100} on:change={onChange} />
+<Modal bind:open={openModal} on:cancel={() => (openModal = false)}>Testing</Modal>
 <div style="padding: 2rem">
   <RangeSlider
     min="100"
@@ -56,14 +29,13 @@
 
   {#each new Array(10) as _}
     <p>
-      Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-      Lorem Ipsum has been the industry's standard dummy text ever since the
-      1500s, when an unknown printer took a galley of type and scrambled it to
-      make a type specimen book. It has survived not only five centuries, but
-      also the leap into electronic typesetting, remaining essentially
-      unchanged. It was popularised in the 1960s with the release of Letraset
-      sheets containing Lorem Ipsum passages, and more recently with desktop
-      publishing software like Aldus PageMaker including versions of Lorem
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+      Ipsum has been the industry's standard dummy text ever since the 1500s, when an
+      unknown printer took a galley of type and scrambled it to make a type specimen book.
+      It has survived not only five centuries, but also the leap into electronic
+      typesetting, remaining essentially unchanged. It was popularised in the 1960s with
+      the release of Letraset sheets containing Lorem Ipsum passages, and more recently
+      with desktop publishing software like Aldus PageMaker including versions of Lorem
       Ipsum.
     </p>
   {/each}
