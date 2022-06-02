9ee74a0437667be918eb2dd3a570631d97131213
Mohamad Harith <mohamadharith.my@gmail.com>
2022-06-02 19:01:03 +0800

    fix: styling

--- components/select/src/Select.svelte
+++ components/select/src/Select.svelte
@@ -138,6 +138,9 @@
       word-wrap: break-word;
       box-shadow: rgba(0, 0, 0, 0.02) 0px 1px 3px 0px,
         rgba(27, 31, 35, 0.15) 0px 0px 0px 1px;
+      max-height: 500px;
+      overflow: hidden;
+      overflow-y: scroll;
 
       &-item {
         cursor: pointer;
