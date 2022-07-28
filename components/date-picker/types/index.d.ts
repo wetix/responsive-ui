f8f7bb8dfa4de53a78460e3ec2b5871e0cf89856
ryantanrk <ryantanrk@gmail.com>
2022-07-28 13:26:43 +0800

    fix: date-picker `form` can use `string` or `HTMLFormElement`

--- components/date-picker/types/index.d.ts
+++ components/date-picker/types/index.d.ts
@@ -11,7 +11,7 @@ export interface DatePickerProps {
   bordered?: boolean;
   useNative?: boolean;
   spanWidth?: boolean;
-  form?: HTMLFormElement;
+  form?: string | HTMLFormElement;
   placeholder?: string;
   min?: string;
   max?: string;
