986c96686882847786917d5f7b36d79f888fbb26
ryantanrk <ryantanrk@gmail.com>
2022-10-21 10:29:39 +0800

    fix: select events

--- components/select/src/MultipleSelect.svelte
+++ components/select/src/MultipleSelect.svelte
@@ -139,7 +139,7 @@
       activeOptionIdx = 0;
     }}
   >
-    <input {name} type="hidden" value={Array.isArray(value) ? value.join(",") : ""} />
+    <input {name} on:input on:change type="hidden" value={Array.isArray(value) ? value.join(",") : ""} />
     <span class="resp-select__tags" on:click={handleRemove}>
       {#each value as item}
         {#if dict.get(item)}
