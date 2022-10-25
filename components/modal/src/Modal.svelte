b7face1246d376f09d2d486755f630235e359c72
ryantanrk <ryantanrk@gmail.com>
2022-10-25 12:15:38 +0800

    fix: modal scroll position

--- components/modal/src/Modal.svelte
+++ components/modal/src/Modal.svelte
@@ -19,7 +19,10 @@
 
   let scrollY = 0;
 
-  $: if (open) {
+  const onOpen = () => {
+    scrollY = window.scrollY;
+
+    dispatch("open");
     const bodyStyle = document.body.getAttribute("style") || "";
     document.body.dataset.style = bodyStyle;
     document.body.dataset.scrollY = scrollY.toString();
@@ -29,7 +32,11 @@
         `position: fixed; top: -${scrollY}px; ${bodyStyle}`
       );
     }, 0);
-  } else {
+  };
+
+  const onClose = () => {
+    dispatch("cancel");
+
     const { style = "" } = document.body.dataset;
     // restore to original style
     document.body.setAttribute("style", style);
@@ -37,10 +44,10 @@
       // restore to position scroll-y
       window.scrollTo(0, scrollY);
     }, 0);
-  }
-</script>
+  };
 
-<svelte:window bind:scrollY />
+  $: open ? onOpen() : onClose();
+</script>
 
 {#if open}
   <div class="resp-modal__box" class:resp-modal--outlined={outlined}>
@@ -49,12 +56,7 @@
         class="resp-modal__overlay"
         in:fade
         out:fade
-        on:click={maskClosable
-          ? () => {
-              open = false;
-              dispatch("cancel");
-            }
-          : undefined}
+        on:click={maskClosable ? () => (open = false) : undefined}
       />
     {/if}
     <div
@@ -74,13 +76,7 @@
       {/if}
       {#if closable}
         <span class="resp-modal__close">
-          <i
-            class="resp-modal__close-icon"
-            on:click={() => {
-              open = false;
-              dispatch("cancel");
-            }}
-          >
+          <i class="resp-modal__close-icon" on:click={() => (open = false)}>
             {@html `<svg viewBox="64 64 896 896" focusable="false" data-icon="close" width="1em" height="1em" fill="currentColor" aria-hidden="true"><path d="M563.8 512l262.5-312.9c4.4-5.2.7-13.1-6.1-13.1h-79.8c-4.7 0-9.2 2.1-12.3 5.7L511.6 449.8 295.1 191.7c-3-3.6-7.5-5.7-12.3-5.7H203c-6.8 0-10.5 7.9-6.1 13.1L459.4 512 196.9 824.9A7.95 7.95 0 00203 838h79.8c4.7 0 9.2-2.1 12.3-5.7l216.5-258.1 216.5 258.1c3 3.6 7.5 5.7 12.3 5.7h79.8c6.8 0 10.5-7.9 6.1-13.1L563.8 512z" /></svg>`}
           </i>
         </span>
